2196b2a278e0ea986b3a77b9a9b9c04fa7ffae73
Nkosinathi-dev1 <nkosinathi.ngele.dev@gmail.com>
2024-04-29 22:29:17 +0200

    ProductCategoryManager (Part 1) in memory CRUD complete

---
+++ MyShop/MyShop.DataAccess.InMemory/ProductCategoryRepository.cs
@@ -0,0 +1,115 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductCategoryRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<ProductCategory> productCategories;
+
+        /// <summary>
+        /// constructort
+        /// assign productCategories with cached list 
+        /// else create a new instance of productCategories
+        /// </summary>
+        public ProductCategoryRepository()
+        {
+            productCategories = cache["productCategories"] as List<ProductCategory>;
+            //create new instrance if null
+            if (productCategories == null)
+            {
+                productCategories = new List<ProductCategory>();
+            }
+        }
+
+        /// <summary>
+        /// commit product in cache
+        /// </summary>
+        public void Commit()
+        {
+            cache["productCategories"] = productCategories;
+        }
+        /// <summary>
+        /// insert new ProductCategory in cache
+        /// </summary>
+        /// <param name="p"></param>
+        public void Insert(ProductCategory p)
+        {
+            productCategories.Add(p);
+        }
+
+        /// <summary>
+        /// find and update ProductCategory in cache
+        /// </summary>
+        /// <param name="product"></param>
+        /// <exception cref="Exception"></exception>
+        public void Update(ProductCategory productCategory)
+        {
+            ProductCategory productToUpdate = productCategories.Find(p => p.Id == productCategory.Category);
+
+            if (productToUpdate != null)
+            {
+                productToUpdate = productCategory;
+            }
+            else
+            {
+                throw new Exception($"Product Category {productCategory.Category} not found");
+            }
+        }
+
+        /// <summary>
+        /// find a single product from cache
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public ProductCategory Find(string id)
+        {
+            ProductCategory productCategory = productCategories.Find(p => p.Id == id);
+
+            if (productCategory != null)
+            {
+                return productCategory;
+            }
+            else
+            {
+                throw new Exception($"Product Category {productCategory.Category} not found");
+            }
+        }
+
+        /// <summary>
+        /// get all productCategories as collection from cache
+        /// </summary>
+        /// <returns></returns>
+        public IQueryable<ProductCategory> Collection()
+        {
+            return productCategories.AsQueryable();
+        }
+
+
+        /// <summary>
+        /// delete a single ProductCategory from cache
+        /// </summary>
+        /// <param name="id"></param>
+        /// <exception cref="Exception"></exception>
+        public void Delete(string id)
+        {
+            ProductCategory productCategoryToDelete = productCategories.Find(p => p.Id == id);
+
+            if (productCategoryToDelete != null)
+            {
+                productCategories.Remove(productCategoryToDelete);
+            }
+            else
+            {
+                throw new Exception($"Product Category {productCategoryToDelete.Category} not found");
+            }
+        }
+    }
+}
---
+++ MyShop/MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -0,0 +1,149 @@
+﻿using MyShop.Core.Models;
+using MyShop.DataAccess.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductCategoryManagerController : Controller
+    {
+        /// <summary>
+        /// instance of ProductCategoryRepository from MyShop.DataAccess.InMemory;
+        /// </summary>
+        ProductCategoryRepository contex;
+
+
+        /// <summary>
+        /// constructor that ProductCategoryRepository 
+        /// </summary>
+        public ProductCategoryManagerController()
+        {
+            contex = new ProductCategoryRepository();
+        }
+        // GET: ProductCategoryManager
+
+        /// <summary>
+        /// get list of productCategoryRepositories from a data source and return them to a index view
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            List<ProductCategory> productCategoryRepositories = contex.Collection().ToList();
+            return View(productCategoryRepositories);
+        }
+
+
+        /// <summary>
+        /// displays the page
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Create()
+        {
+            ProductCategory productCategory = new ProductCategory();
+            return View(productCategory);
+        }
+
+        /// <summary>
+        /// validate 
+        /// if data is not valid display validation message on the same page
+        /// if data is valid save to the source and redirect to index
+        /// </summary>
+        /// <param name="productCategory"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Create(ProductCategory productCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(productCategory);
+            }
+
+            contex.Insert(productCategory);
+            contex.Commit();
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// find by id
+        /// if null return not found
+        /// retrun productCategory if not null
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Edit(string id)
+        {
+            ProductCategory productCategory = contex.Find(id);
+            if (productCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productCategory);
+        }
+
+        /// <summary>
+        /// find product by id
+        /// validate data
+        /// commit and redirect
+        /// </summary>
+        /// <param name="productCategory"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Edit(ProductCategory productCategory, string id)
+        {
+            ProductCategory productCategoryToEndit = contex.Find(id);
+            if (productCategoryToEndit == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(productCategory);
+            }
+
+            productCategoryToEndit.Category = productCategory.Category;
+
+
+            contex.Commit();
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// find productCategory by id then view productCategory to frlrtr
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Delete(string id)
+        {
+            ProductCategory productCategoryToDelete = contex.Find(id);
+            if (productCategoryToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productCategoryToDelete);
+        }
+
+        /// <summary>
+        /// cormirm delete the redirect to index
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string id)
+        {
+            ProductCategory productCategoryToDelete = contex.Find(id);
+            if (productCategoryToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            contex.Delete(id);
+            contex.Commit();
+            return RedirectToAction("Index");
+        }
+    }
+}
