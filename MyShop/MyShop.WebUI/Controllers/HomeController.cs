6d2d8b319a08b160030fe39d951e3dad613b12b4
Nkosinathi-dev1 <nkosinathi.ngele.dev@gmail.com>
2024-06-29 11:13:04 +0200

    Added Product Listing

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,34 @@ namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+
+        //instance of IRepository<> interface
+        IRepository<Product> contex;
+        IRepository<ProductCategory> contexCategories;
+
+        // constructor 
+        public HomeController(IRepository<Product> contex, IRepository<ProductCategory> contexCategories)
+        {
+            this.contex = contex;
+            this.contexCategories = contexCategories;
+        }
+
+
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = contex.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string id)
+        {
+            Product product = contex.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(product);
         }
 
         public ActionResult About()
