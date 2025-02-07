b1932265651372ceb2e71e226e2e602be675e44e
nkosinathi <nathi@sethala.com>
2025-02-08 00:00:18 +0200

    Added UserType enum

---
+++ MyShop/MyShop.Core/Enums/UserType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Enums
+{
+    public enum UserType
+    {
+        /// <summary>
+        /// Job seeker who can apply for positions
+        /// </summary>
+        Candidate = 0,
+
+        /// <summary>
+        /// Human Resources professional belonging to a company
+        /// </summary>
+        HR = 1,
+
+        /// <summary>
+        /// Talent acquisition specialist (can be independent or company-affiliated)
+        /// </summary>
+        Recruiter = 2,
+
+        /// <summary>
+        /// System administrator with full privileges
+        /// </summary>
+        SuperAdmin = 3
+    }
+}
